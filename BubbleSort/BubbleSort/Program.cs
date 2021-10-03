6a954dc3f7dc7107c3c06c0363fd28eddbcc63e3
Koshmanov Ilya <k0shmanov@yandex.ru>
2021-10-03 21:23:55 +0300

    bubblesort

---
+++ BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> array = new List<int>(){3,2,1,0, -1, -2, -3};
+            foreach (var value in array)
+            {
+                Console.Write($"{value} \t");
+            }
+            BubbleSort(array);
+            Console.WriteLine();
+            foreach (var value in array)
+            {
+                Console.Write($"{value} \t");
+            }
+            Console.ReadLine();
+        }
+        static void BubbleSort(List<int> loop)
+        {
+            int tmp = default;
+            for (int i = loop.Count - 1; i >= 0; i--)
+            {
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (loop[i] < loop[j])
+                    {
+                        tmp = loop[i];
+                        loop[i] = loop[j];
+                        loop[j] = tmp;
+                    }
+                }
+            }
+        }
+    }
+}
