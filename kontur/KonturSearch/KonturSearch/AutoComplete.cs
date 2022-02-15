caf14cc5d8649f304a578d69daac91cccd1c7171
Koshmanov Ilya <k0shmanov@yandex.ru>
2022-02-15 23:57:58 +0300

    Задание  1 от контура, реализация поиска
    Не доделан поиск по Имени и Отчеству

---
+++ kontur/KonturSearch/KonturSearch/AutoComplete.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace AutoComplete
+{
+    public struct FullName
+    {
+        public string Name;
+        public string Surname;
+        public string Patronymic;
+    }
+    public class AutoCompleter
+    {       
+        StringBuilder tempFullName = new StringBuilder();
+        LinkedList<string> linkedListForSearching = default;
+        public void AddToSearch(List<FullName> fullNames)
+        {   //Добавьте новый элемент чтобы он начал участвовать в поиске по фио         
+            List<string> listForSearching = new List<string>();
+            foreach (var fullName in fullNames)
+            {
+                if (fullName.Surname != null)
+                {
+                    tempFullName.Append(fullName.Surname + " ");
+                }
+                if (fullName.Name != null)
+                {
+                    tempFullName.Append(fullName.Name + " ");
+                }
+                if (fullName.Patronymic != null)
+                {
+                    tempFullName.Append(fullName.Patronymic);
+                }
+                listForSearching.Add(tempFullName.ToString());
+                tempFullName.Clear();
+            }
+            listForSearching.Sort();
+            linkedListForSearching = new LinkedList<string>(listForSearching);            
+        }  
+        public List<string> Search(string prefix)
+        {
+            //Реализуйте алгоритм поиска по префиксу фио, при этом фио может состоять только из имени или фамилии и имени или отчества или фамилии и отчества или всё вместе.
+            List<string> outputFullNameList = new List<string>();            
+            if (prefix.Length > 101)
+            {
+                throw new ArgumentException("Long request name.");
+            }
+            if (string.IsNullOrWhiteSpace(prefix))
+            {
+                throw new ArgumentException("Empty request.");
+            }
+
+            foreach (var stringСompare in linkedListForSearching)
+            {                
+                if (stringСompare.Substring(0, prefix.Length).ToString().Contains(prefix))
+                {
+                    outputFullNameList.Add(stringСompare);
+                }
+            }
+            return outputFullNameList;
+        }    
+    }
+}
+
+//Переданное ФИО может содержать между фамилией и именем или именем и отчеством множество пробелов. Количество пробелов не должно влиять на поиск.
+
+//Ради упрощения, решили что автокомплит будет искать только по префиксу, без исправления ошибок и без поиска по подстроке.
+
+
---
+++ kontur/KonturSearch/KonturSearch/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoComplete;
+
+namespace AutoComplete
+{
+    internal class Program
+    {       
+        static void Main(string[] args)
+        {
+            FullName a0 = new FullName() { Name = "123456", Surname = "ewertyurtg", Patronymic = "3" };
+            FullName a1 = new FullName() { Name = "11", Surname = "ajfghjfghjs", Patronymic = "3" };
+            FullName a2 = new FullName() { Name = "111", Surname = "dsjfghfefenfgha", Patronymic = "3" };
+            FullName a3 = new FullName() { Name = "1111", Surname = "fejrfdgje56r", Patronymic = "3" };            
+            FullName a4 = new FullName() { Name = "1123", Surname = "fedsnfgfehue4rfg", Patronymic = "3" };
+            FullName a5 = new FullName() { Name = "11234", Surname = "cvxu56dtjfgx", Patronymic = "3" };
+            FullName a6 = new FullName() { Name = "1523", Surname = "ujyfghjrtyt", Patronymic = "3" };
+            FullName a7 = new FullName() { Name = "16543", Surname = "iuuyrtyuuy", Patronymic = "3" };
+            FullName a8 = new FullName() { Name = "1856", Surname = "ertrtyy2", Patronymic = "3" };
+            FullName a9 = new FullName() { Name = "19657", Surname = "feqscrtfeyutre", Patronymic = "3" };            
+            FullName a10 = new FullName() { Name = "176578", Surname = "jhfbdcfgfehjxg", Patronymic = "3" };
+            FullName a11 = new FullName() { Name = "11111", Surname = "weuertdfefg", Patronymic = "3" };
+            FullName a12 = new FullName() { Name = "176578", Surname = "sdftydfghjjfe2", Patronymic = "3" };
+            FullName a13 = new FullName() { Name = "11111", Surname = "dfgyeuertdfefg", Patronymic = "3" };
+            FullName a14 = new FullName() { Name = "11111", Surname = "ityuiweuertdfefg", Patronymic = "3" };
+            FullName a15 = new FullName() { Name = "11111", Surname = "k,jhweuertdfefg", Patronymic = "3" };
+            FullName a16 = new FullName() { Name = "11111", Surname = "bnmweuertdfefg", Patronymic = "3" };
+            FullName a17 = new FullName() { Name = "11111", Surname = "vbnmweuertdfefg", Patronymic = "3" };
+            FullName a18 = new FullName() { Name = "11111", Surname = "wvmnbeuertdfefg", Patronymic = "3" };
+            FullName a19 = new FullName() { Name = "11111", Surname = "wvmnbeuertdfefg", Patronymic = "3" };
+
+
+            AutoCompleter ac = new AutoCompleter();
+            List<FullName> fn = new List<FullName>(){ a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19 };
+            ac.AddToSearch(fn);
+            //ac.TestInput();
+            List<string> list = ac.Search("111");
+            foreach (string s in list)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadKey();
+        }
+    }
+}
