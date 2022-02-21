2c11dfb08f7b812568fa7b3819e6240481c0020a
Koshmanov Ilya <k0shmanov@yandex.ru>
2022-02-21 08:02:57 +0300

    Использование dictionary для решения задачи
    Пропуски в поиске, разобраться почему

--- kontur/KonturSearch/KonturSearch/AutoComplete.cs
+++ kontur/KonturSearch/KonturSearch/AutoComplete.cs
@@ -10,31 +10,36 @@ namespace AutoComplete
         public string Surname;
         public string Patronymic;
         public override string ToString()
-        { 
-            if (Name == null && Patronymic != null)
-            {
-                return string.Join(" ", Surname?.Trim(), Patronymic?.Trim()).Trim();
-            }
-            else
-            {
-                return string.Join(" ", Surname?.Trim(), Name?.Trim(), Patronymic?.Trim()).Trim();
-            }           
+        {
+            return string.Join(" ", Surname?.Trim(), Name?.Trim(), Patronymic?.Trim()).Trim();
         }
     }
     public class AutoCompleter
     {
-        List<string> listForSearching = new List<string>();
+        string tempKey = default;
+        Dictionary<string, List<string>> fullNameDictionary = new Dictionary<string, List<string>>();
         List<string> outputFullNameList = new List<string>();
         public void AddToSearch(List<FullName> fullNames)
         {
             for (int i = 0; i < fullNames.Count; i++)
             {
-                listForSearching.Add(fullNames[i].ToString());                
+                tempKey = fullNames[i].ToString().Substring(0, 1);
+                if (fullNameDictionary.ContainsKey(tempKey) != true)
+                {
+                    fullNameDictionary.Add(tempKey, new List<string>() { fullNames[i].ToString() });
+                    Console.WriteLine("Значение добавлено ключа нет"); //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+                }
+                else
+                {
+                    fullNameDictionary[tempKey].Add(fullNames[i].ToString());
+                    Console.WriteLine("Значение добавлено ключ есть"); //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+                }
             }
         }
         public List<string> Search(string prefix)
         {
-
+            tempKey = prefix.Substring(0, 1);
+            Console.WriteLine(tempKey + " " + tempKey.Length); //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
             if (prefix.Length > 101)
             {
                 throw new ArgumentException("Long request name.");
@@ -43,14 +48,21 @@ namespace AutoComplete
             {
                 throw new ArgumentException("Empty request.");
             }
-            for (int i = 0; i < listForSearching.Count; i++)
-            {
-                if (prefix.Contains(listForSearching[i].Substring(0, prefix.Length)))
+            for (int i = 0; i < fullNameDictionary.Count; i++)
+            { 
+                if (fullNameDictionary.ContainsKey(tempKey))
                 {
-                    outputFullNameList.Add(listForSearching[i]);
+                    for (int j = 0; j < fullNameDictionary[tempKey].Count; j++)
+                    {
+                        if (fullNameDictionary[tempKey][j].Substring(0, prefix.Length) == prefix)
+                        {
+                            outputFullNameList.Add(fullNameDictionary[tempKey][j]);
+                        }
+                    }
                 }
             }
+            Console.WriteLine(outputFullNameList.Count);
             return outputFullNameList;
         }
-    }    
+    }
 }
