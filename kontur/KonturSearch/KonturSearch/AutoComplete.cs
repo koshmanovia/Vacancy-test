2dee9e70ed82554c690b841c0c0bb94e89bb74dd
Koshmanov Ilya <k0shmanov@yandex.ru>
2022-02-23 23:35:15 +0300

    Рабочий вариант, реализовано бинарное дерево
    тесты проходит, после сдачи всех заданий причесать

--- kontur/KonturSearch/KonturSearch/AutoComplete.cs
+++ kontur/KonturSearch/KonturSearch/AutoComplete.cs
@@ -6,61 +6,49 @@ namespace AutoComplete
 {
     public struct FullName
     {
-        internal string stringFullName;
-        private int fullNameHash;
         public string Name;
         public string Surname;
         public string Patronymic;
         public override string ToString()
         {
-            stringFullName = string.Join(" ", Surname?.Trim(), Name?.Trim(), Patronymic?.Trim()).Trim();
-            return stringFullName;
-        }
-        public int GetHash()
-        {
-            if (stringFullName == null)
-            {
-                stringFullName = GetFullNameString();
-            }
-            if (fullNameHash == 0)
-            {
-                fullNameHash = stringFullName[0].GetHashCode();
-            }
-            return fullNameHash;
-        }
-        public string GetFullNameString()
-        {
-            if (stringFullName == null)
-            {
-                stringFullName = ToString();
-            }
-            return stringFullName;
+            return string.Join(" ", Surname?.Trim(), Name?.Trim(), Patronymic?.Trim()).Trim();
         }
     }
     public class AutoCompleter
     {
         int tempArrayCount = default;
-        int tempKey = default;
-        Dictionary<int, List<string>> fullNameDictionary = new Dictionary<int, List<string>>();
-        List<string> outputFullNameList = new List<string>();
+        int tempKeyExternalDictionary = default;
+        int tempKeyInternalDictionary = default;
+        Dictionary<int, Dictionary<int, List<string>>> fullNameDictionary = new Dictionary<int, Dictionary<int, List<string>>>();       
         public void AddToSearch(List<FullName> fullNames)
         {
             tempArrayCount = fullNames.Count;
             for (int i = 0; i < tempArrayCount; i++)
             {
-                tempKey = fullNames[i].GetHash();
-                if (fullNameDictionary.ContainsKey(tempKey))
+                tempKeyExternalDictionary = fullNames[i].ToString()[0].GetHashCode();
+                tempKeyInternalDictionary = fullNames[i].ToString()[1].GetHashCode();
+
+                if (fullNameDictionary.ContainsKey(tempKeyExternalDictionary))
                 {
-                    fullNameDictionary[tempKey].Add(fullNames[i].GetFullNameString());
+                    if (fullNameDictionary[tempKeyExternalDictionary].ContainsKey(tempKeyInternalDictionary))
+                    {
+                        fullNameDictionary[tempKeyExternalDictionary][tempKeyInternalDictionary].Add(fullNames[i].ToString());
+                    }
+                    else
+                    {
+                        fullNameDictionary[tempKeyExternalDictionary].Add(tempKeyInternalDictionary, new List<string>() { fullNames[i].ToString() });
+                    }
                 }
                 else
                 {
-                    fullNameDictionary.Add(tempKey, new List<string>() { fullNames[i].GetFullNameString() });
+                    fullNameDictionary.Add(tempKeyExternalDictionary, new Dictionary<int, List<string>>());
+                    fullNameDictionary[tempKeyExternalDictionary].Add(tempKeyInternalDictionary, new List<string>() { fullNames[i].ToString() });
                 }
             }
         }
         public List<string> Search(string prefix)
         {
+           var outputFullNameList = new List<string>();
             if (prefix.Length > 100)
             {
                 throw new ArgumentException("Long request name.");
@@ -69,23 +57,47 @@ namespace AutoComplete
             {
                 throw new ArgumentException("Empty request.");
             }
-            tempKey = prefix[0].GetHashCode();
-            outputFullNameList = new List<string>();
-            if (fullNameDictionary.ContainsKey(tempKey))
+
+            tempKeyExternalDictionary = prefix[0].GetHashCode();
+            if (prefix.Length > 1)
             {
-                tempArrayCount = fullNameDictionary[tempKey].Count;
-                for (int i = 0; i < tempArrayCount; i++)
+                tempKeyInternalDictionary = prefix[1].GetHashCode();
+                if (fullNameDictionary.ContainsKey(tempKeyExternalDictionary))
                 {
-                    try
+
+                    if (fullNameDictionary[tempKeyExternalDictionary].ContainsKey(tempKeyInternalDictionary))
                     {
-                        if (fullNameDictionary[tempKey][i].StartsWith(prefix))
+
+                        tempArrayCount = fullNameDictionary[tempKeyExternalDictionary][tempKeyInternalDictionary].Count;
+                        for (int i = 0; i < tempArrayCount; i++)
                         {
-                            outputFullNameList.Add(fullNameDictionary[tempKey][i]);
+                            try
+                            {
+                                if (fullNameDictionary[tempKeyExternalDictionary][tempKeyInternalDictionary][i].StartsWith(prefix))
+                                {
+                                    outputFullNameList.Add(fullNameDictionary[tempKeyExternalDictionary][tempKeyInternalDictionary][i]);
+                                }
+                            }
+                            catch (System.ArgumentOutOfRangeException)
+                            {
+                                continue;
+                            }
                         }
                     }
-                    catch (System.ArgumentOutOfRangeException)
+                }
+            }
+            else
+            {
+                int countArray = fullNameDictionary[tempKeyExternalDictionary].Count;
+                List<int> keyInternalDictionary = new List<int>(fullNameDictionary[tempKeyExternalDictionary].Keys);
+                for (int i = 0; i < countArray; i++)
+                {
+                    if (fullNameDictionary[tempKeyExternalDictionary].ContainsKey(keyInternalDictionary[i]))
                     {
-                        continue;
+                        for (int j = 0; j < fullNameDictionary[tempKeyExternalDictionary][keyInternalDictionary[i]].Count; j++)
+                        {
+                            outputFullNameList.Add(fullNameDictionary[tempKeyExternalDictionary][keyInternalDictionary[i]][j]);
+                        }
                     }
                 }
             }
