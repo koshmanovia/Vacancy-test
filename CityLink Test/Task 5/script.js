098e28042c86dc2d21a48b9f33d142bfbd166c9b
Koshmanov Ilya <k0shmanov@yandex.ru>
2023-10-10 19:39:36 +0300

    edit struct project

---
+++ CityLink Test/Task 5/script.js
@@ -0,0 +1,17 @@
+<div class="container" >
+      <div class="row">
+        <div class="col-md-8 mx-auto">
+          <h2>Олимпиада</h2>
+        </div>
+      </div>
+    </div>     
+
+    <div class="card">            
+        <div class="card-body">
+          <h5 class="card-title">Участники</h5>
+          <p class="card-text">введите имена участников через запятую.</p>
+          <input type="text" name="name" /><br />
+          <p> </p>
+          <a href="#" class="btn btn-primary">Добавить</a>
+        </div>
+      </div>
