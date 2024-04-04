fd84e493a152f6499fcbe0ee4916062fb16cdadd
Koshmanov Ilya <k0shmanov@yandex.ru>
2024-04-04 19:16:55 +0300

    CreateProject

---
+++ Read_IP/DataBaseIP.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Read_IP
+{
+    internal class DataBaseIP
+    {
+        /*<БЫЛ!! Реализован словарь IP Адресов Возможно будет время, допилю
+         * 
+         *  словарь 1 актет > 
+         *  словарь 2 актет > 
+         *  словарь 3 актет > 
+         *  словарь 4 актет > массив времени
+         * 
+         * Резонный вопрос, для чего так сложно и громоздко? 
+         * А для поиска, когда прочитаем весь лог, 
+         * и растусуем по словарю, выводить можно быстрее и легче
+         * нужные диапазоны. В расчете на то, что можем запустить 
+         * несколько выборок за одну сессию программы. Это еще не самое наркоманское, 
+         * сначала думал делать это побитово, но передумал изза перевода из двоичной 
+         * в десятичную при записи, съело бы много ресурсов
+        
+       
+        int i = 0;
+        //private List<string> data = new List<string>();
+        private Dictionary<int, Dictionary<int, Dictionary<int, Dictionary<int, List<DateTime>>>>> db
+            = new Dictionary<int, Dictionary<int, Dictionary<int, Dictionary<int, List<DateTime>>>>>();
+        public Dictionary<int, Dictionary<int, Dictionary<int, Dictionary<int, List<DateTime>>>>> DB { get { return db; } }
+        public static void AddIP(string ip)
+        {
+            if(CheckIP.Check(ip))
+            {
+                i++;
+            }
+        }
+        public static void AddIPAndDate(string data)
+        {
+
+        } */
+
+        private Dictionary<string, List<DateTime>> _data;
+
+        public Dictionary<string, List<DateTime>> Data 
+        {  
+            get { return _data; } 
+            set { _data = value; }
+        }
+
+        public void AddData(string key, DateTime value)
+        { 
+            if (CheckIP.Check(key))
+            {            
+                if (Data.ContainsKey(key))
+                {
+                    Data[key].Add(value);
+                }
+                else 
+                {
+                    Data.Add(key, new List<DateTime>(){value});
+                }
+            }
+            else { throw new ArgumentException ("Не правильный формат IP адреса"); }
+        }
+        public List<DateTime> GetData(string key) 
+        {
+            if (CheckIP.Check(key))
+            {
+                return Data[key];
+            }
+            else { throw new ArgumentException("Не правильный формат IP адреса"); }            
+        }
+    
+    }
+}
---
+++ Read_IP/Read_IP.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Read_IP
+{
+    internal class Read_IP
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
